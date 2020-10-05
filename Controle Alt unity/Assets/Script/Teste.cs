3f41754d164298c7138718d43bbea6c60b3946ad
Guillaume <g.roge@rubika-edu.com>
2020-10-05 14:13:55 +0200

    Uduino import

---
+++ Controle Alt unity/Assets/Script/Teste.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Uduino;
+
+public class Teste : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        UduinoManager.Instance.pinMode(2, PinMode.Output);
+        StartCoroutine(LoopArduino());
+    }
+
+    IEnumerator LoopArduino()
+    {
+        yield return null;
+    }
+}
