6b9be297a1d1490fbb5fc5ebafbfe568b25c3176
Guillaume <g.roge@rubika-edu.com>
2020-10-05 14:53:08 +0200

    Save mael

---
+++ Controle Alt unity/Assets/Scripts/Meteor/MeteoriteController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteoriteController : MonoBehaviour
+{
+    [Header("Enemy Attributes")]
+    [SerializeField] float speed = 5f;
+    [SerializeField] float rotateSpeed = 50f;
+    [SerializeField] bool canRotate;
+    bool canMove = true;
+
+    [Space]
+    [Header("Enemy Stats")]
+    public float bound_X = -11f;
+
+    private void Start()
+    {
+        if (canRotate)
+        {
+            if (Random.Range(0, 2) > 0)
+            {
+                rotateSpeed = Random.Range(rotateSpeed, rotateSpeed + 20f);
+                rotateSpeed *= -1f;
+            }
+            else
+            {
+                rotateSpeed = Random.Range(rotateSpeed, rotateSpeed = 20f);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        MoveEnemy();
+        RotateEnemy();
+    }
+
+    void MoveEnemy()
+    {
+        if (canMove)
+        {
+            Vector3 temp = transform.position;
+            temp.x -= speed * Time.deltaTime;
+            transform.position = temp;
+
+            if (temp.x < bound_X)
+                Destroy(gameObject, 1);
+
+
+        }
+    }
+
+    void RotateEnemy()
+    {
+        if (canRotate)
+        {
+            transform.Rotate(new Vector3(0f, 0f, rotateSpeed * Time.deltaTime), Space.World);
+        }
+    }
+
+    private void DestroyEnemy()
+    {
+
+        Destroy(gameObject, 0.1f);
+    }
+}
---
+++ Controle Alt unity/Assets/Scripts/Paralax/scrollSpaceBG.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrollSpaceBG : MonoBehaviour
+{
+    [SerializeField] float scrollSpeed = 0.1f;
+    float xScroll;
+
+    private MeshRenderer meshRenderer;
+
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    private void Update()
+    {
+        Scroll();
+    }
+
+    private void Scroll()
+    {
+        xScroll = Time.time * scrollSpeed;
+
+        Vector2 offset = new Vector2(xScroll, 0f);
+        meshRenderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
+    }
+}
