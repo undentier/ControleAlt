6f9669081eb50a5c204088300e23a2072f6bf79e
maelRicou <m.ricou@rubika-edu.com>
2020-10-06 15:45:53 +0100

    Shooter Spaceship
    il est dans le spawner de asteroids car on a toujours pas le système de vague par vague voili voilu ah et il me manque de gérer sa rotation pour qu'il regarde le joueur

---
+++ Controle Alt unity/Assets/Scripts/Ennemies/ShootingShipController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingShipController : MonoBehaviour
+{
+    [Header("Shooter References")]
+    Rigidbody rb;
+    public Transform shootPoint;
+    [SerializeField] GameObject bullet;
+    Transform target;
+
+
+
+    [Space]
+    [Header("Shooter Stats - Move Section")]
+    [SerializeField] float stopDis = 10f;
+    [SerializeField] float shooterRadar;
+    [SerializeField] float shooterRange;
+    [SerializeField] float shooterSpeed = 5;
+    [SerializeField] float retreatDis;
+
+
+
+    [Space]
+    [Header("Shooter Stats - Shoot Section")]
+    float timeBtwShots;
+    public float startTimeBtwShots;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        timeBtwShots = startTimeBtwShots;
+      
+    }
+
+    private void Update()
+    {
+
+        float distance = Vector3.Distance(transform.position, target.position);  //on compare la postion entre le shooter et le joueur
+
+        if (distance > stopDis && distance > shooterRadar) //si la distance entre les deux vaisseaux est supérieur à la zone d'arrêt et la zone de detection de l'enemy
+        {
+            
+            transform.position = Vector3.MoveTowards(transform.position, target.position, shooterSpeed * Time.deltaTime); //il avance en direction du joueur
+
+        }
+        else if (distance > stopDis && distance > retreatDis) //si l'enemy est proche du joueur mais pas au point de devoir reculer alors il s'arrête
+        {
+            rb.velocity = Vector3.zero;
+        }
+        else if (distance > retreatDis) //sinon il recule
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, -shooterSpeed * Time.deltaTime); //recule vers la direction opposé du joueur
+        }
+
+        if (timeBtwShots <= 0 && distance < shooterRange)  //on check si le joueur est en range + si le cooldown permet de tirer
+        {
+            Instantiate(bullet, transform.position, Quaternion.identity);
+            timeBtwShots = startTimeBtwShots;
+
+            //ajouter le son
+            //mmmh pourquoi pas petite lumière
+        }
+        else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
+
+
+        //rotate le ship par rapport au transform du joueur
+
+    }
+
+    //ici on trace juste une sphere rouge pour checker sa zone de detection
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, stopDis);
+    }
+
+
+}
--- Controle Alt unity/Assets/Scripts/Meteor/AsteroidManager.cs
+++ Controle Alt unity/Assets/Scripts/Meteor/AsteroidManager.cs
@@ -17,7 +17,7 @@ public class AsteroidManager : MonoBehaviour
 
     [Space]
     [Header("Stats")]
-    public float timer = 2f;
+    public float timer = 2f; //le fameux timer pour varier les spawns, il peut être modulable pour augmenter la difficulté mais vu qu'on part sur un système de vague jsp pourquoi j'écris ça
 
     private void Start()
     {
@@ -27,6 +27,8 @@ public class AsteroidManager : MonoBehaviour
 
     void Spawner()
     {
+
+        //ici je crée juste une range dans l'axe Z pour être sûr que les enemis spawnent pas en dehors de la cam
         float pos_Z = Random.Range(eBoundary.miniZ, eBoundary.maxiZ);
         Vector3 temp = transform.position;
         temp.z = pos_Z;
@@ -37,7 +39,7 @@ public class AsteroidManager : MonoBehaviour
         }
         else
         {
-            //Instantiate(shooter, temp, Quaternion.Euler(0f, 0f, 90f));
+            Instantiate(shooter, temp, Quaternion.Euler(0f, 0f, 0f));
         }
 
         Invoke("Spawner", timer);
