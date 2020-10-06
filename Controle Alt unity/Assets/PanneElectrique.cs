086b0bc5c2409c28fdcf1e09ac97b558198000b3
Lucas <l.deutschmann@rubika-edu.com>
2020-10-06 15:16:49 +0200

    panne de lights

---
+++ Controle Alt unity/Assets/PanneElectrique.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+/// <summary>
+/// Deutschmann Lucas
+/// 
+/// Panne d'electricité
+/// </summary>
+
+namespace Panne
+{
+    public class PanneElectrique : MonoBehaviour
+    {
+        public float Opacity;
+        bool panne;
+        bool IsActive;
+        bool baseActivityState;
+        public string inputPlayer1;
+        public string inputPlayer2;
+        public Image panel;
+
+        Color newColor;
+        Color baseColor;
+        // Start is called before the first frame update
+        void Start()
+        {
+            baseColor = panel.color;
+            newColor = panel.color;
+            newColor.a = Opacity;
+
+            IsActive = GetComponent<RandomEventGeneration>().isActive;
+            baseActivityState = GetComponent<RandomEventGeneration>().isActive;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            IsActive = GetComponent<RandomEventGeneration>().isActive;
+
+            if (IsActive != baseActivityState)
+            {
+                lightsOff();
+            }
+            if (Input.GetKey(inputPlayer1) && Input.GetKey(inputPlayer2))
+            {
+                FixLights();
+            }
+
+
+            if (panne)
+            {
+                panel.color = newColor;
+            }
+            else
+            {
+                panel.color = baseColor;
+            }
+
+
+        }
+
+        void lightsOff()
+        {
+            panne = true;
+
+        }
+        void FixLights()
+        {
+            panne = false;
+        }
+    }
+}
+
---
+++ Controle Alt unity/Assets/RandomEventGeneration.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Panne;
+public class RandomEventGeneration : MonoBehaviour
+{
+    public bool isActive;
+    public string input;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(input))
+        {
+            isActive = true;
+        }
+        if (Input.GetKeyUp(input))
+        {
+            isActive = false;
+        }
+    }
+}
