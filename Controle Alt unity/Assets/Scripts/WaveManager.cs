a66073731ce6b85e0064a8176fc24ad99b5af8b1
Guillaume <g.roge@rubika-edu.com>
2020-10-06 11:40:46 +0200

    Debut waveManager

---
+++ Controle Alt unity/Assets/Scripts/WaveManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Management;
+
+
+public class WaveManager : Singleton<WaveManager>
+{
+    public int numberKamikaze;
+    public int numberShoter;
+
+
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
