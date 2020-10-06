7f754c05a34df01062eb950dba170775d2fed63d
Guillaume <g.roge@rubika-edu.com>
2020-10-06 17:11:24 +0200

    Base du systeme de spawn

---
+++ Controle Alt unity/Assets/Scripts/Wave Systeme/Spawner.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Manager;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] spawnerPoint;
+
+    public bool canCoroutine = true;
+
+
+    private int kamikaze;
+    private int shoter;
+
+
+    public GameObject kamikazeObject;
+    public GameObject shoterObject;
+       
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        if (WaveManager.Instance.start == true)
+        {
+            if (WaveManager.Instance.canSpawn == true)
+            {
+                if (canCoroutine == true)
+                {
+                    canCoroutine = false;
+
+                    kamikaze = WaveManager.Instance.numberKamikaze;
+                    shoter = WaveManager.Instance.numberShoter;
+
+                    StartCoroutine(SpawnerSysteme());
+                }
+            }
+        }
+    }
+
+    IEnumerator SpawnerSysteme()
+    {
+        for (int i = 0; i < (kamikaze+shoter) ; i++)
+        {
+            int wichPoint = Random.Range(0, spawnerPoint.Length);
+
+            GameObject ship = Instantiate(kamikazeObject, spawnerPoint[wichPoint].transform.position, transform.rotation);
+            WaveManager.Instance.counter.Add(ship);
+
+            yield return new WaitForSeconds(WaveManager.Instance.timeBtwSpawn);
+        }
+
+        canCoroutine = true;
+
+        WaveManager.Instance.canSpawn = false;
+        WaveManager.Instance.canNextWave = true;
+    }
+
+}
---
+++ Controle Alt unity/Assets/Scripts/Wave Systeme/WaveManager.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Manager
+{
+    public class WaveManager : MonoBehaviour
+    {
+        public static WaveManager Instance;
+
+        public List<GameObject> counter = new List<GameObject>();
+
+        public bool waveActive = false;
+        public bool canSpawn = false;
+        public bool start;
+        public bool canNextWave = false;
+
+        public int numberKamikaze;
+        public int numberShoter;
+
+        public int numberOfWave;
+
+        public float timeBtwSpawn;
+
+        public float timeBtwEmergency;
+        public float chanceOfEmergency;
+
+        public int kamikazeAdd;
+        public int shoterAdd;
+
+        public float EmergencyChanceAdd;
+        public float EmergencyTimeAdd;
+
+        public float timeBtwWave;
+
+        void Start()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        void Update()
+        {
+            if (Input.GetButtonDown("Fire"))
+            {
+                if (start == false)
+                {
+                    start = true;
+                }
+            }
+
+            counter.RemoveAll(list_item => list_item == null);
+
+
+            if (start == true)
+            {
+                if (waveActive == false)
+                {
+                    waveActive = true;
+                    StartCoroutine(ActiveWave());
+                }
+            }
+
+            if (counter.Count == 0)
+            {
+                if (start == true)
+                {
+                    if (waveActive == true)
+                    {
+                        if (canNextWave == true)
+                        {
+                            canNextWave = false;
+                            waveActive = false;
+                        }
+                    }
+                }
+            }
+        }
+
+        IEnumerator ActiveWave()
+        {
+            yield return new WaitForSeconds(timeBtwWave);
+
+            numberOfWave += 1;
+
+            numberKamikaze += kamikazeAdd;
+            numberShoter += shoterAdd;
+
+            canSpawn = true;
+        }
+    }
+}
