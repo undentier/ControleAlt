a79deec8688d7d22120b85abb6e26f933b516d0e
maelRicou <m.ricou@rubika-edu.com>
2020-10-06 10:44:46 +0100

    Shooter Spaceship 1.0
    ce n'est que le début

---
+++ Controle Alt unity/Assets/Scripts/ShootingShipController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingShipController : MonoBehaviour
+{
+    [Header("Shooter References")]
+    Rigidbody rb;
+    [SerializeField] float shooterSpeed = 5;
+    bool shootingPhase = false;
+    public float lookRadius = 10f;
+    Transform target;
+    public float stopDistance = 5f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        //target = FindGame
+      
+    }
+
+    private void Update()
+    {
+
+        if (shootingPhase == false)
+        {
+            rb.velocity = transform.forward * shooterSpeed;
+            
+            if (Vector3.Distance(transform.position, target.position) > stopDistance)
+            {
+
+            }
+        }
+        else if (shootingPhase)
+        {
+
+        }
+
+
+
+
+    }
+
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+
+
+}
