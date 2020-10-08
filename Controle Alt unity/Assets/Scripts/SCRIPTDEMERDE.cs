88e2c8c72dcc40cd143b38a0102b3805dcd73886
Matis <m.duperray@rubika-edu.com>
2020-10-08 11:49:07 +0200

    Hangar
    C'est giga méga stylé

--- Controle Alt unity/Assets/Scripts/SCRIPTDEMERDE.cs
+++ Controle Alt unity/Assets/Scripts/SCRIPTDEMERDE.cs
@@ -5,11 +5,40 @@ using UnityEngine;
 public class SCRIPTDEMERDE : MonoBehaviour
 {
     public AK.Wwise.Event decollageAudio;
+    public GameObject stars;
 
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody rb;
+    bool canStart = true;
+
+
+    private void Start()
     {
-        decollageAudio.Post(gameObject);
+        rb = this.GetComponent<Rigidbody>();
+        stars.SetActive(false);
     }
 
+    // Start is called before the first frame update
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && canStart)
+        {
+            StartCoroutine(StartGame());
+        }
+    }
+
+
+    IEnumerator StartGame()
+    {
+        canStart = false;
+        decollageAudio.Post(gameObject);
+        rb.AddForce(-100, 0, 0);
+        yield return new WaitForSeconds(17);
+        stars.SetActive(true);
+        rb.AddForce(-10000, 0, 0);
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
 }
