742f671a2acc0324f2480e184f990051fc3aace4
lucas <l.deutschmann@rubika-edu.com>
2020-10-13 12:20:13 +0200

    Main menu rough

---
+++ Controle Alt unity/Assets/Scripts/UI/StartGame.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartGame : MonoBehaviour
+{
+    public Text inputFieldText;
+    public Text errorText;
+    
+    private void Start()
+    {
+        errorText.enabled = false;
+    }
+
+    public void NewGame(string sceneName)
+    {
+        if (inputFieldText.text.Length > 0)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+        else
+        {
+            errorText.enabled = true;
+            Debug.Log("Pas de nom");
+        }
+        
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
